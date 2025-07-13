86d03d9b90141f180f303a125da10d99f8e383f0
DenaUxUi <root@DELLG155511.>
2025-07-13 17:30:16 +0600

    some fixes

--- Example_14/main.go
+++ Example_14/main.go
@@ -3,15 +3,15 @@ package main
 import (
     "context"
     "encoding/json"
-    
     "fmt"
     "log"
     "net/http"
     "os"
 
-    	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/ec2"
+    "github.com/aws/aws-sdk-go-v2/aws"
+    "github.com/aws/aws-sdk-go-v2/config"
+    "github.com/aws/aws-sdk-go-v2/service/ec2"
+    ec2Types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 )
 
 var (
@@ -19,13 +19,11 @@ var (
     lastInstanceId string
 )
 
-
 type Response struct {
     Message string `json:"system-response"`
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-    // Разрешаем CORS
     w.Header().Set("Access-Control-Allow-Origin", "*")
     w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
     w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
@@ -54,87 +52,87 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(response)
 }
 
-func main() {
-    // Отдача статики из папки static при запросе к /
-    http.Handle("/", http.FileServer(http.Dir("./static")))
-    
-    // API обработчик
-    http.HandleFunc("/api/hello", helloHandler)
-
-    fmt.Println("Server started at http://localhost:8080")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        fmt.Println("Error starting server:", err)
-    }
-
-    cfg, err := config.LoadDefaultConfig(context.TODO(),
-    	config.WithRegion(os.Getenv("AWS_REGION")),
-    )
-    if err != nil {
-	log.Fatalf("Error of cfg AWS %v", err)
-    }
-
-    ec2Client = ec2.NewFromConfig(cfg)
-
-    http.HandlerFunc("/api/aws/vm/create", handleCreateInstance)
-    http.HandlerFunc("/api/aws/vm.terminate", handleTerminateInstance)
-
-}
-
-func handleCreateInstance(w http.ResponceWriter, r *http.Request){
+func handleCreateInstance(w http.ResponseWriter, r *http.Request) {
     ami := os.Getenv("AMI_ID")
     keyName := os.Getenv("KEY_NAME")
-    instanceType := "t3.micro"
+    // instanceType := "t3.micro"
 
-    input := &ec2.RunInstanceInput{
-	ImageId: aws.String(ami),
-	InstanceType: ec2.InstanceType(instanceType),
-	MinCount: aws.Int32(1),
-	MaxCount: aws.Int32(1),
-	KeyName: aws.String(keyName),
-    } 
-
-    result, err := ec2Client.RunInstance(context.TODO(), input)
-    if err != nil || len(result.Instances) == 0 {
-	http.Error(w, "Instance creation error: "+err.Error(), http.StatusInternalServerError)
-	return
+    input := &ec2.RunInstancesInput{
+        ImageId:      aws.String(ami),
+        InstanceType: ec2Types.InstanceType("t3.micro"),
+        MinCount:     aws.Int32(1),
+        MaxCount:     aws.Int32(1),
+        KeyName:      aws.String(keyName),
     }
 
-    lastInstanceId = *result.Instance[0].InstanceId
-    log.Println("Created new instance; ", lastInstanceId)
+    result, err := ec2Client.RunInstances(context.TODO(), input)
+    if err != nil || len(result.Instances) == 0 {
+        http.Error(w, "Instance creation error: "+err.Error(), http.StatusInternalServerError)
+        return
+    }
 
-    resp:=map[string]string{
-	"instanceId": lastInstanceId,
-	"state":      string(result.Instances[0].State.Name),
+    lastInstanceId = *result.Instances[0].InstanceId
+    log.Println("Created new instance:", lastInstanceId)
+
+    resp := map[string]string{
+        "instanceId": lastInstanceId,
+        "state":      string(result.Instances[0].State.Name),
     }
     json.NewEncoder(w).Encode(resp)
 }
 
-func handleTerminateInstance(w http.ResponseWriter, r *http.request){
-    if lastInstanceid == "" {
-	http.Error(w, "We not found active instance", http.StatusBadRequest)
-	return
+func handleTerminateInstance(w http.ResponseWriter, r *http.Request) {
+    if lastInstanceId == "" {
+        http.Error(w, "We not found active instance", http.StatusBadRequest)
+        return
     }
 
-    input := &ec2.TerminateInstanceInput{
-	InstanceIds: []string{lastInstanceId},
+    input := &ec2.TerminateInstancesInput{
+        InstanceIds: []string{lastInstanceId},
     }
 
     result, err := ec2Client.TerminateInstances(context.TODO(), input)
     if err != nil {
-	http.Error(w, "Error at termination: "+err.Error(), http.StatusInternalServerError)
-	return
+        http.Error(w, "Error at termination: "+err.Error(), http.StatusInternalServerError)
+        return
     }
 
-    state := result.Terminatinginstance[0].CurrentState.Name
-    log.Println("Instance is over: ", lastInstanceId)
+    state := result.TerminatingInstances[0].CurrentState.Name
+    log.Println("Instance terminated:", lastInstanceId)
 
     resp := map[string]string{
-	"terminated": lastInstanceId,
-	"state":      string(state),
+        "terminated": lastInstanceId,
+        "state":      string(state),
     }
     json.NewEncoder(w).Encode(resp)
 
     lastInstanceId = ""
+}
+
+func main() {
+    // 1. AWS SDK init
+    cfg, err := config.LoadDefaultConfig(context.TODO(),
+        config.WithRegion(os.Getenv("AWS_REGION")),
+    )
+    if err != nil {
+        log.Fatalf("Error loading AWS config: %v", err)
+    }
+
+    ec2Client = ec2.NewFromConfig(cfg)
+
+    // 2. Handlers
+    http.Handle("/", http.FileServer(http.Dir("./static")))
+    http.HandleFunc("/api/hello", helloHandler)
+    http.HandleFunc("/api/aws/vm/create", handleCreateInstance)
+    http.HandleFunc("/api/aws/vm/terminate", handleTerminateInstance)
+
+    // 3. Start server
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
+    fmt.Println("Server started at http://localhost:" + port)
+    log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
